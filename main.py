c992c3e24eae31ffddd0496ed8ba8cbf5f594da0
jdgutirod <jdgutierrezr08@gmail.com>
2025-12-04 23:52:26 -0500

    fixed main app

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def create_app():
     app.include_router(views.router)
     return app
 
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     uvicorn.run(app, host="0.0.0.0", port=8080)
